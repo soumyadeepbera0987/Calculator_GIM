19931855adb599974394191c51615a3013bb0352
Soumyadeep Bera <167670541+soumyadeepbera0987@users.noreply.github.com>
2025-08-03 11:42:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,10 +14,15 @@ Array.from(buttons).forEach((button)=>{
         string = string + '*' ;
         document.querySelector('input').value= string;
     }
+    else if(e.target.innerHTML == '%'){
+        string = string + '/100';
+        document.querySelector('input').value = string;
+    }
     else{ 
     console.log(e.target)
     string = string + e.target.innerHTML;
     document.querySelector('input').value = string;
       }
   })
+
 })
